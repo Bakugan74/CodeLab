495d7986967e1ebc0d141650633d7038d939cf15
marcotozzy <sergey.erokhin2003@bk.ru>
2024-03-22 18:38:15 +0700

    Добавил frame4 и исправил layout

---
+++ src/components/Frame4/Frame4.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Frame4.scss';
+
+const Frame4 = () => {
+    return(
+        <div className='frame4'>
+
+            <div className='frame4__block'>
+                <div>
+                    <img className='frame4__block__image' src="https://s3-alpha-sig.figma.com/img/8b56/b93a/c5ec0b26d2d6d84db4411022535bfb87?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gTJB04ITcsRuIl2G0CTDarUjX1CINx~--YL6xzQmJ2aneY6wJ4I39sIFG~PggERjKMvyO5abUgjIqgbYsrw~rkJmLhl57ZnuilIAIF2f6aqj~-gOXD~Hjfm9~xk8PwJ3fJYPHx3AJk3E1peQCU-5dYBy382sA~s2W-5KOV58PhFlXb0ABJtQ6j3XOxjxLgnxUKVNCjVHbJXOA2i0Yqv9u8-KFwuAq3IYRKJ6qJTeTw8nG7z-j7xICsC2gSEJvSW0nNp2PHATIAUyhiPGTIfYMqz7NKuFHnFY9y5Xdo-jL-eNgAP-X4kkuKzTKGPBVgPtgwosofIi~cpsEF2025ea3w__" alt="" />
+                </div>
+                <div className='frame4__block__title'>
+                    <p>Оценка способностей</p>
+                </div>
+                <div>
+                    <img  className='frame4__block__star' src="http://cdn.onlinewebfonts.com/svg/img_397133.png" alt="" />
+                </div>
+                <div className='frame4__block__description'>
+                    <p>Пройдите вводный курс в начальном этапе, для оценки вашего уровня развития в данной сфере на начальном этапе.</p>
+                </div>
+            </div>
+                
+            <div className='frame4__block'>
+                <div>
+                    <img className='frame4__block__image' src="https://s3-alpha-sig.figma.com/img/db73/eef2/96104d2febe40773acf2aeb84bd2cafc?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oH~9spo7CC9t81CckOxO-QGzU~noIgrmLPuWJz~ZEDPoiceNS2eu~4oIsTa7Nx2XV8h9WMGqCbsrVd7Tuibi6y3mdZXiWb4RJ3H2dLbho073XVCWr2vdANLoPDWsUvLQKBrKqICl5EcZWJnyOWqQ3oPeJM8MtRssk8servfvZQwbsvjKPR94Vmk-d3FEzZu4Ews~9glFMY9pWvdU4RLHKSGHZdPsaSfvhs8AxJGr9m2PVReYzuDXCvR8Jhaxa5-3ruWXU7c7TVy3CMDgGRFVK3~fVrIWu~N4OOGxoo5CB36TXVJEpvqSqp4htYaw5O8TLs05vO4newIcgnjaWwBjug__" alt="" />
+                </div>
+                <div className='frame4__block__title'>
+                    <p>Оценка способностей</p>
+                </div>
+                <div>
+                    <img  className='frame4__block__star' src="http://cdn.onlinewebfonts.com/svg/img_397133.png" alt="" />
+                </div>
+                <div className='frame4__block__description'>
+                    <p>Пройдите вводный курс в начальном этапе, для оценки вашего уровня развития в данной сфере на начальном этапе.</p>
+                </div>
+            </div>
+
+            <div className='frame4__block'>
+                <div>
+                    <img className='frame4__block__image' src="https://s3-alpha-sig.figma.com/img/0b8a/592f/54cc35c11f9e088f358e610ec6d8db20?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BecnHFeRjIGi8utRkHMMzYuprH7TLmK~pFkliD5Qx~W0tktcC0Jk2AOAASKW5pxUPIZhzjTZqrLbL08hZ~kotthIZFvJZ3afnQmvfaJoRDDmB0csTm0spu6SxbRq5Aw7I5lg~7rXpp7R1WZXALsIfkt1N~82YEO0NhgBzVBZMZjjwjdOQzTZBwWe0REvY-CXHDz4IKD13znvoEGhTqh-2R1WBxrJmAzTRToLCY3taQcH4jDn1aUwyrMoW58XNqbqjJUjPI~m8hdjHVUf634EI4Zr7L8dMUxIcOU2Gww4fRcJ~q-dRFy8yLkgraJF8~2ET3Yvpcp1gJvg2Gwg7jhTMw__" alt="" />
+                </div>
+                <div className='frame4__block__title'>
+                    <p>Оценка способностей</p>
+                </div>
+                <div>
+                    <img  className='frame4__block__star' src="http://cdn.onlinewebfonts.com/svg/img_397133.png" alt="" />
+                </div>
+                <div className='frame4__block__description'>
+                    <p>Пройдите вводный курс в начальном этапе, для оценки вашего уровня развития в данной сфере на начальном этапе.</p>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Frame4;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,12 +2,17 @@ import {} from 'react-router-dom';
 import './Layout.scss';
 import Effect from '../Frame2/Effect.jsx';
 import Header from '../Header/Header.jsx';
+import Frame4 from '../Frame4/Frame4.jsx';
+
 
 const Layout = () => {
 	return (
 		<>
 			<Header />
             <Effect />
+			<Frame4 />
+			
+
 		</>
 	);
 };
